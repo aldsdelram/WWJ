0702f7c076cbafd266076cd1d192fd5f7a4f30c1
skubbsgit <skubbs.git@gmail.com>
2017-01-25 15:33:35 +0800

    add modal for adding to cart

--- themes/wwj/includes/modal-templates.php
+++ themes/wwj/includes/modal-templates.php
@@ -63,3 +63,23 @@
 <?php endif; ?>
 
 
+
+
+
+
+<div class="portal--modal add_to_cart_modal_container" style="display: hidden;">
+	<div class="portal--modal-details">
+		<div class="portal--modal-content">
+			<p>
+				<img src="<?= get_template_directory_uri() . '/img/star.png'?>">
+				<img src="<?= get_template_directory_uri() . '/img/star.png'?>">
+				<img src="<?= get_template_directory_uri() . '/img/star.png'?>">
+			</p>
+			<h3> ITEM SUCCESSFULLY ADDED TO YOUR CART </h3>
+			<div class="btn-panel">
+				<a href="<?= home_url('checkout')?>" class="yes">CHECKOUT</a>
+				<a href="#" class="no">BACK</a>
+			</div>
+		</div>
+	</div>
+</div>
